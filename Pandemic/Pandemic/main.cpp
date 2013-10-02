dfbc7e8bb7acd677baafd87f997f1de818f0915a
aaquino3 <Ardee@Ardees-MacBook-Pro.local>
2013-10-02 08:36:14 -0700

    Initial Commit

---
+++ Pandemic/Pandemic/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Pandemic
+//
+//  Created by Ardee Aquino on 10/2/13.
+//  Copyright (c) 2013 Ardee Aquino. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
