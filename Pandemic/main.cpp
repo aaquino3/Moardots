b06fb69ca3cd77affde6007d3c9424ee5615d091
aaquino3 <Ardee@Ardees-MacBook-Pro.local>
2013-10-03 15:00:30 -0700

    changing main

--- Pandemic/main.cpp
+++ Pandemic/main.cpp
@@ -1,20 +1,11 @@
-//
-//  main.cpp
-//  Pandemic
-//
-//  Created by Ardee Aquino on 10/2/13.
-//  Copyright (c) 2013 Ardee Aquino. All rights reserved.
-//
-
 #include <iostream>
 
 using namespace std;
 
-
 int main(int argc, const char * argv[])
 {
 
-    // insert code here...
+    //main hello world
     cout << "Hello" << endl;
     cout << "my name is ardee aquino" << endl;
     cout << "I am doing this from a browser." << endl;
