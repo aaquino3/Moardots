7eed46e342422e56861b370b2b9fbe6667505958
aaquino3 <Ardee@Ardees-MacBook-Pro.local>
2013-10-09 12:42:53 -0700

    deleting stuff out of main

--- Pandemic/main.cpp
+++ Pandemic/main.cpp
@@ -8,12 +8,6 @@ int main(int argc, const char * argv[])
 
     //main hello world
     cout << "Hello" << endl;
-    cout << "my name is ardee aquino" << endl;
-    cout << "I am doing this from a browser." << endl;
-	cout << "THis is a test not from browser." << endl;
-    cout << "I am doing this from a browser." << endl;
-    cout << "I am doing this from a browser." << endl;
-	cout << "Hello my name is Vasiliy Rodin" << endl;
 
     return 0;
 }
