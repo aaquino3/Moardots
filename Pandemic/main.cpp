9b220492c652d5bf36289e2cf296a9bd40b6b7eb
aaquino3 <aquino.ardee@gmail.com>
2013-10-09 23:43:14 -0700

    deleting lines, testing eclipse

--- Pandemic/main.cpp
+++ Pandemic/main.cpp
@@ -11,9 +11,6 @@ int main(int argc, const char * argv[])
     cout << "Hello" << endl;
     cout << "my name is ardee aquino" << endl;
     cout << "I am doing this from a browser." << endl;
-	cout << "THis is a test not from browser." << endl;
-    cout << "I am doing this from a browser." << endl;
-    cout << "I am doing this from a browser." << endl;
 	cout << "Hello my name is Vasiliy Rodin" << endl;
 
     return 0;
