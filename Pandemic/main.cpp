9f614c0b3fe2431ce3d3521445bc4d2bb8e6dd6b
aaquino3 <aquino.ardee@gmail.com>
2013-10-10 14:32:56 -0700

    adding pdf....dont know if we really need it. its the pdf from blackboard

--- Pandemic/main.cpp
+++ Pandemic/main.cpp
@@ -9,7 +9,5 @@ int main(int argc, const char * argv[])
     cout << "Hello" << endl;
     cout << "my name is ardee aquino" << endl;
     cout << "testing eclipse" << endl;
-    cout << "Testing eclipse again" << endl;
-
     return 0;
 }
