20d9884f42f9c51490f98dfad824a257a9b749b9
aaquino3 <aquino.ardee@gmail.com>
2013-10-09 23:53:35 -0700

    testing eclipse

--- Pandemic/main.cpp
+++ Pandemic/main.cpp
@@ -11,7 +11,6 @@ int main(int argc, const char * argv[])
     cout << "Hello" << endl;
     cout << "my name is ardee aquino" << endl;
     cout << "I am doing this from a browser." << endl;
-	cout << "Hello my name is Vasiliy Rodin" << endl;
 
     return 0;
 }
