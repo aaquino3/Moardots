eab4e115bbbef776b0afc58290e163c21957173b
ED <reyone59@gmail.com>
2013-10-15 02:16:11 -0700

    Deck driver update

--- Pandemic/Deck Driver Program/main.cpp
+++ Pandemic/Deck Driver Program/main.cpp
@@ -2,17 +2,23 @@
 #include "Deck.h"
 #include "Card.h"
 #include <string>
+#include <ctime>
+#include <cstdlib>
 using namespace std;
 
 int main()
 {
-    int sizeOfDeck = 2;
+	// random seeder
+	srand(unsigned(time(0)));
+
+    int sizeOfDeck = 7;
     Deck roleDeck(sizeOfDeck);
     Card temp;
     roleDeck.readInCards("cards.txt", '|');
+	roleDeck.shuffle();
     for(int i = 0; i < sizeOfDeck; i++)
     {
-        temp = roleDeck.dealCard(i);
+        temp = roleDeck.dealCard();
         string type = temp.getType();
         string name = temp.getName();
         string description = temp.getDescription();
@@ -21,5 +27,7 @@ int main()
         cout << "name: " << name << endl;
         cout << "Description: \n\n" << description << endl;
     }
+
+	getchar();
     return 0;
 }
