efea59115daf51a0fbde20c9a3f9d59b34ef7066
ransackeld <kcransa@gmail.com>
2013-10-16 21:15:03 -0700

    Created Dice.h
    Added Dice.h - about to add Dice.cpp

---
+++ Pandemic/Dice.h
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+#ifndef DICE_H
+#define DICE_H
+
+class Dice
+{
+private:
+  int random_num1, random_num2;
+  int lownum, highnum;
+  int range;
+  
+public:
+  Dice();
+};
+#endif
+  
+
+
