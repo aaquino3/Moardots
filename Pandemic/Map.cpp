9046f1364006efadf1a5a2a5a7c76bbc929b4e8c
ed <reyone59@gmail.com>
2013-10-25 23:52:02 -0700

    add stubs for game Map

---
+++ Pandemic/Map.cpp
@@ -0,0 +1,49 @@
+/*
+ * Map.cpp
+ *
+ *  Created on: Oct 25, 2013
+ *      Author: ed
+ *
+ *  Using throws to return error when used with try/catch blocks
+ *  throw will return -1 for an error.
+ */
+
+#include "Map.h"
+
+Map::Map() {
+	// TODO Auto-generated constructor stub
+
+}
+
+
+void Map::pushBackCity(City city)
+{
+	//need to do
+}
+
+void Map::popBackCity()
+{
+	// need to do
+}
+int Map::GetNumberOfCities()
+{
+	// need to do
+	return 0;
+}
+
+// will return element of specified location
+City Map::getCity(int loc)
+{
+	// need to do
+	return 0;
+}
+
+City Map::getCity(string cityName)
+{
+	// need to do
+	return 0;
+}
+Map::~Map() {
+	// TODO Auto-generated destructor stub
+}
+
