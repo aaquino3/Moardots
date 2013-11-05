a9fec0b2afcc286b6ee279a52d3a2729a71d72b1
VasiliyRodin <rodinvasiliy@gmail.com>
2013-11-04 19:40:56 -0800

    Something is being commited in order for me to sync

---
+++ build-Pandemic-Desktop_Qt_5_1_1_MinGW_32bit-Debug/ui_mainwindow.h
@@ -0,0 +1,318 @@
+/********************************************************************************
+** Form generated from reading UI file 'mainwindow.ui'
+**
+** Created by: Qt User Interface Compiler version 5.1.1
+**
+** WARNING! All changes made in this file will be lost when recompiling UI file!
+********************************************************************************/
+
+#ifndef UI_MAINWINDOW_H
+#define UI_MAINWINDOW_H
+
+#include <QtCore/QVariant>
+#include <QtWidgets/QAction>
+#include <QtWidgets/QApplication>
+#include <QtWidgets/QButtonGroup>
+#include <QtWidgets/QHeaderView>
+#include <QtWidgets/QLabel>
+#include <QtWidgets/QMainWindow>
+#include <QtWidgets/QMenuBar>
+#include <QtWidgets/QPushButton>
+#include <QtWidgets/QStatusBar>
+#include <QtWidgets/QToolBar>
+#include <QtWidgets/QWidget>
+
+QT_BEGIN_NAMESPACE
+
+class Ui_MainWindow
+{
+public:
+    QWidget *centralWidget;
+    QLabel *Board;
+    QPushButton *SanFransisco;
+    QPushButton *LasAngeles;
+    QPushButton *Chicago;
+    QPushButton *Montreal;
+    QPushButton *NewYork;
+    QPushButton *Washington;
+    QPushButton *Miami;
+    QPushButton *MexicoCity;
+    QPushButton *Atlanta;
+    QPushButton *Bogdta;
+    QPushButton *Lima;
+    QPushButton *SaoPaulo;
+    QPushButton *BuenosAires;
+    QPushButton *Santiago;
+    QPushButton *Madrid;
+    QPushButton *London;
+    QPushButton *Essen;
+    QPushButton *StPetersburg;
+    QPushButton *Paris;
+    QPushButton *Milan;
+    QPushButton *Algiers;
+    QPushButton *Cairo;
+    QPushButton *Lagos;
+    QPushButton *Kinshasa;
+    QPushButton *Johannesburg;
+    QPushButton *Khartoum;
+    QPushButton *Riyadh;
+    QPushButton *Istanbul;
+    QPushButton *Baghdad;
+    QPushButton *Moscow;
+    QPushButton *Karachi;
+    QPushButton *Delhi;
+    QPushButton *Mumbai;
+    QPushButton *Chennai;
+    QPushButton *Kolkata;
+    QPushButton *Tehran;
+    QPushButton *Beijing;
+    QPushButton *Shanghai;
+    QPushButton *Seoul;
+    QPushButton *Tokyo;
+    QPushButton *Osaka;
+    QPushButton *Bangkok;
+    QPushButton *HongKong;
+    QPushButton *Taipei;
+    QPushButton *Jakartha;
+    QPushButton *HoChiMinhCity;
+    QPushButton *Manila;
+    QPushButton *Sydney;
+    QMenuBar *menuBar;
+    QToolBar *mainToolBar;
+    QStatusBar *statusBar;
+
+    void setupUi(QMainWindow *MainWindow)
+    {
+        if (MainWindow->objectName().isEmpty())
+            MainWindow->setObjectName(QStringLiteral("MainWindow"));
+        MainWindow->resize(1201, 900);
+        centralWidget = new QWidget(MainWindow);
+        centralWidget->setObjectName(QStringLiteral("centralWidget"));
+        Board = new QLabel(centralWidget);
+        Board->setObjectName(QStringLiteral("Board"));
+        Board->setGeometry(QRect(0, 0, 1201, 851));
+        Board->setPixmap(QPixmap(QString::fromUtf8(":/new/images/board.jpg")));
+        SanFransisco = new QPushButton(centralWidget);
+        SanFransisco->setObjectName(QStringLiteral("SanFransisco"));
+        SanFransisco->setGeometry(QRect(50, 260, 111, 32));
+        LasAngeles = new QPushButton(centralWidget);
+        LasAngeles->setObjectName(QStringLiteral("LasAngeles"));
+        LasAngeles->setGeometry(QRect(70, 390, 101, 32));
+        Chicago = new QPushButton(centralWidget);
+        Chicago->setObjectName(QStringLiteral("Chicago"));
+        Chicago->setGeometry(QRect(170, 230, 71, 32));
+        Montreal = new QPushButton(centralWidget);
+        Montreal->setObjectName(QStringLiteral("Montreal"));
+        Montreal->setGeometry(QRect(250, 230, 81, 32));
+        NewYork = new QPushButton(centralWidget);
+        NewYork->setObjectName(QStringLiteral("NewYork"));
+        NewYork->setGeometry(QRect(330, 240, 81, 32));
+        Washington = new QPushButton(centralWidget);
+        Washington->setObjectName(QStringLiteral("Washington"));
+        Washington->setGeometry(QRect(330, 310, 101, 32));
+        Miami = new QPushButton(centralWidget);
+        Miami->setObjectName(QStringLiteral("Miami"));
+        Miami->setGeometry(QRect(300, 390, 61, 32));
+        MexicoCity = new QPushButton(centralWidget);
+        MexicoCity->setObjectName(QStringLiteral("MexicoCity"));
+        MexicoCity->setGeometry(QRect(150, 420, 101, 32));
+        Atlanta = new QPushButton(centralWidget);
+        Atlanta->setObjectName(QStringLiteral("Atlanta"));
+        Atlanta->setGeometry(QRect(200, 330, 71, 32));
+        Bogdta = new QPushButton(centralWidget);
+        Bogdta->setObjectName(QStringLiteral("Bogdta"));
+        Bogdta->setGeometry(QRect(290, 460, 71, 32));
+        Lima = new QPushButton(centralWidget);
+        Lima->setObjectName(QStringLiteral("Lima"));
+        Lima->setGeometry(QRect(190, 580, 61, 32));
+        SaoPaulo = new QPushButton(centralWidget);
+        SaoPaulo->setObjectName(QStringLiteral("SaoPaulo"));
+        SaoPaulo->setGeometry(QRect(400, 590, 91, 32));
+        BuenosAires = new QPushButton(centralWidget);
+        BuenosAires->setObjectName(QStringLiteral("BuenosAires"));
+        BuenosAires->setGeometry(QRect(340, 650, 111, 32));
+        Santiago = new QPushButton(centralWidget);
+        Santiago->setObjectName(QStringLiteral("Santiago"));
+        Santiago->setGeometry(QRect(190, 670, 81, 32));
+        Madrid = new QPushButton(centralWidget);
+        Madrid->setObjectName(QStringLiteral("Madrid"));
+        Madrid->setGeometry(QRect(420, 300, 71, 32));
+        London = new QPushButton(centralWidget);
+        London->setObjectName(QStringLiteral("London"));
+        London->setGeometry(QRect(470, 180, 71, 32));
+        Essen = new QPushButton(centralWidget);
+        Essen->setObjectName(QStringLiteral("Essen"));
+        Essen->setGeometry(QRect(560, 170, 61, 32));
+        StPetersburg = new QPushButton(centralWidget);
+        StPetersburg->setObjectName(QStringLiteral("StPetersburg"));
+        StPetersburg->setGeometry(QRect(600, 150, 115, 32));
+        Paris = new QPushButton(centralWidget);
+        Paris->setObjectName(QStringLiteral("Paris"));
+        Paris->setGeometry(QRect(500, 250, 61, 32));
+        Milan = new QPushButton(centralWidget);
+        Milan->setObjectName(QStringLiteral("Milan"));
+        Milan->setGeometry(QRect(620, 220, 61, 32));
+        Algiers = new QPushButton(centralWidget);
+        Algiers->setObjectName(QStringLiteral("Algiers"));
+        Algiers->setGeometry(QRect(500, 340, 71, 32));
+        Cairo = new QPushButton(centralWidget);
+        Cairo->setObjectName(QStringLiteral("Cairo"));
+        Cairo->setGeometry(QRect(590, 370, 61, 32));
+        Lagos = new QPushButton(centralWidget);
+        Lagos->setObjectName(QStringLiteral("Lagos"));
+        Lagos->setGeometry(QRect(530, 430, 61, 32));
+        Kinshasa = new QPushButton(centralWidget);
+        Kinshasa->setObjectName(QStringLiteral("Kinshasa"));
+        Kinshasa->setGeometry(QRect(530, 530, 81, 32));
+        Johannesburg = new QPushButton(centralWidget);
+        Johannesburg->setObjectName(QStringLiteral("Johannesburg"));
+        Johannesburg->setGeometry(QRect(590, 620, 115, 32));
+        Khartoum = new QPushButton(centralWidget);
+        Khartoum->setObjectName(QStringLiteral("Khartoum"));
+        Khartoum->setGeometry(QRect(660, 450, 91, 32));
+        Riyadh = new QPushButton(centralWidget);
+        Riyadh->setObjectName(QStringLiteral("Riyadh"));
+        Riyadh->setGeometry(QRect(690, 420, 71, 32));
+        Istanbul = new QPushButton(centralWidget);
+        Istanbul->setObjectName(QStringLiteral("Istanbul"));
+        Istanbul->setGeometry(QRect(580, 280, 71, 32));
+        Baghdad = new QPushButton(centralWidget);
+        Baghdad->setObjectName(QStringLiteral("Baghdad"));
+        Baghdad->setGeometry(QRect(680, 300, 81, 32));
+        Moscow = new QPushButton(centralWidget);
+        Moscow->setObjectName(QStringLiteral("Moscow"));
+        Moscow->setGeometry(QRect(690, 210, 81, 32));
+        Karachi = new QPushButton(centralWidget);
+        Karachi->setObjectName(QStringLiteral("Karachi"));
+        Karachi->setGeometry(QRect(770, 330, 71, 32));
+        Delhi = new QPushButton(centralWidget);
+        Delhi->setObjectName(QStringLiteral("Delhi"));
+        Delhi->setGeometry(QRect(830, 300, 61, 32));
+        Mumbai = new QPushButton(centralWidget);
+        Mumbai->setObjectName(QStringLiteral("Mumbai"));
+        Mumbai->setGeometry(QRect(770, 430, 71, 32));
+        Chennai = new QPushButton(centralWidget);
+        Chennai->setObjectName(QStringLiteral("Chennai"));
+        Chennai->setGeometry(QRect(820, 480, 71, 32));
+        Kolkata = new QPushButton(centralWidget);
+        Kolkata->setObjectName(QStringLiteral("Kolkata"));
+        Kolkata->setGeometry(QRect(880, 320, 71, 32));
+        Tehran = new QPushButton(centralWidget);
+        Tehran->setObjectName(QStringLiteral("Tehran"));
+        Tehran->setGeometry(QRect(780, 260, 71, 32));
+        Beijing = new QPushButton(centralWidget);
+        Beijing->setObjectName(QStringLiteral("Beijing"));
+        Beijing->setGeometry(QRect(880, 250, 71, 32));
+        Shanghai = new QPushButton(centralWidget);
+        Shanghai->setObjectName(QStringLiteral("Shanghai"));
+        Shanghai->setGeometry(QRect(890, 290, 81, 32));
+        Seoul = new QPushButton(centralWidget);
+        Seoul->setObjectName(QStringLiteral("Seoul"));
+        Seoul->setGeometry(QRect(1040, 230, 61, 32));
+        Tokyo = new QPushButton(centralWidget);
+        Tokyo->setObjectName(QStringLiteral("Tokyo"));
+        Tokyo->setGeometry(QRect(1100, 270, 61, 32));
+        Osaka = new QPushButton(centralWidget);
+        Osaka->setObjectName(QStringLiteral("Osaka"));
+        Osaka->setGeometry(QRect(1090, 360, 61, 32));
+        Bangkok = new QPushButton(centralWidget);
+        Bangkok->setObjectName(QStringLiteral("Bangkok"));
+        Bangkok->setGeometry(QRect(880, 390, 81, 32));
+        HongKong = new QPushButton(centralWidget);
+        HongKong->setObjectName(QStringLiteral("HongKong"));
+        HongKong->setGeometry(QRect(940, 360, 91, 32));
+        Taipei = new QPushButton(centralWidget);
+        Taipei->setObjectName(QStringLiteral("Taipei"));
+        Taipei->setGeometry(QRect(1040, 390, 61, 32));
+        Jakartha = new QPushButton(centralWidget);
+        Jakartha->setObjectName(QStringLiteral("Jakartha"));
+        Jakartha->setGeometry(QRect(880, 550, 81, 32));
+        HoChiMinhCity = new QPushButton(centralWidget);
+        HoChiMinhCity->setObjectName(QStringLiteral("HoChiMinhCity"));
+        HoChiMinhCity->setGeometry(QRect(914, 500, 131, 32));
+        Manila = new QPushButton(centralWidget);
+        Manila->setObjectName(QStringLiteral("Manila"));
+        Manila->setGeometry(QRect(1060, 490, 61, 32));
+        Sydney = new QPushButton(centralWidget);
+        Sydney->setObjectName(QStringLiteral("Sydney"));
+        Sydney->setGeometry(QRect(1080, 670, 71, 32));
+        MainWindow->setCentralWidget(centralWidget);
+        menuBar = new QMenuBar(MainWindow);
+        menuBar->setObjectName(QStringLiteral("menuBar"));
+        menuBar->setGeometry(QRect(0, 0, 1201, 22));
+        MainWindow->setMenuBar(menuBar);
+        mainToolBar = new QToolBar(MainWindow);
+        mainToolBar->setObjectName(QStringLiteral("mainToolBar"));
+        MainWindow->addToolBar(Qt::TopToolBarArea, mainToolBar);
+        statusBar = new QStatusBar(MainWindow);
+        statusBar->setObjectName(QStringLiteral("statusBar"));
+        MainWindow->setStatusBar(statusBar);
+
+        retranslateUi(MainWindow);
+
+        QMetaObject::connectSlotsByName(MainWindow);
+    } // setupUi
+
+    void retranslateUi(QMainWindow *MainWindow)
+    {
+        MainWindow->setWindowTitle(QApplication::translate("MainWindow", "MainWindow", 0));
+        Board->setText(QString());
+        SanFransisco->setText(QApplication::translate("MainWindow", "San Fransisco", 0));
+        LasAngeles->setText(QApplication::translate("MainWindow", "Las Angeles", 0));
+        Chicago->setText(QApplication::translate("MainWindow", "Chicago", 0));
+        Montreal->setText(QApplication::translate("MainWindow", "Montreal", 0));
+        NewYork->setText(QApplication::translate("MainWindow", "New York", 0));
+        Washington->setText(QApplication::translate("MainWindow", "Washington", 0));
+        Miami->setText(QApplication::translate("MainWindow", "Miami", 0));
+        MexicoCity->setText(QApplication::translate("MainWindow", "Mexico City", 0));
+        Atlanta->setText(QApplication::translate("MainWindow", "Atlanta", 0));
+        Bogdta->setText(QApplication::translate("MainWindow", "Bogdta", 0));
+        Lima->setText(QApplication::translate("MainWindow", "Lima", 0));
+        SaoPaulo->setText(QApplication::translate("MainWindow", "Sao Paulo", 0));
+        BuenosAires->setText(QApplication::translate("MainWindow", "Buenos Aires", 0));
+        Santiago->setText(QApplication::translate("MainWindow", "Santiago", 0));
+        Madrid->setText(QApplication::translate("MainWindow", "Madrid", 0));
+        London->setText(QApplication::translate("MainWindow", "London", 0));
+        Essen->setText(QApplication::translate("MainWindow", "Essen", 0));
+        StPetersburg->setText(QApplication::translate("MainWindow", "St Petersburg", 0));
+        Paris->setText(QApplication::translate("MainWindow", "Paris", 0));
+        Milan->setText(QApplication::translate("MainWindow", "Milan", 0));
+        Algiers->setText(QApplication::translate("MainWindow", "Algiers", 0));
+        Cairo->setText(QApplication::translate("MainWindow", "Cairo", 0));
+        Lagos->setText(QApplication::translate("MainWindow", "Lagos", 0));
+        Kinshasa->setText(QApplication::translate("MainWindow", "Kinshasa", 0));
+        Johannesburg->setText(QApplication::translate("MainWindow", "Johannesburg", 0));
+        Khartoum->setText(QApplication::translate("MainWindow", "Khartoum", 0));
+        Riyadh->setText(QApplication::translate("MainWindow", "Riyadh", 0));
+        Istanbul->setText(QApplication::translate("MainWindow", "Istanbul", 0));
+        Baghdad->setText(QApplication::translate("MainWindow", "Baghdad", 0));
+        Moscow->setText(QApplication::translate("MainWindow", "Moscow", 0));
+        Karachi->setText(QApplication::translate("MainWindow", "Karachi", 0));
+        Delhi->setText(QApplication::translate("MainWindow", "Delhi", 0));
+        Mumbai->setText(QApplication::translate("MainWindow", "Mumbai", 0));
+        Chennai->setText(QApplication::translate("MainWindow", "Chennai", 0));
+        Kolkata->setText(QApplication::translate("MainWindow", "Kolkata", 0));
+        Tehran->setText(QApplication::translate("MainWindow", "Tehran", 0));
+        Beijing->setText(QApplication::translate("MainWindow", "Beijing", 0));
+        Shanghai->setText(QApplication::translate("MainWindow", "Shanghai", 0));
+        Seoul->setText(QApplication::translate("MainWindow", "Seoul", 0));
+        Tokyo->setText(QApplication::translate("MainWindow", "Tokyo", 0));
+        Osaka->setText(QApplication::translate("MainWindow", "Osaka", 0));
+        Bangkok->setText(QApplication::translate("MainWindow", "Bangkok", 0));
+        HongKong->setText(QApplication::translate("MainWindow", "Hong Kong", 0));
+        Taipei->setText(QApplication::translate("MainWindow", "Taipei", 0));
+        Jakartha->setText(QApplication::translate("MainWindow", "Jakartha", 0));
+        HoChiMinhCity->setText(QApplication::translate("MainWindow", "Ho Chi Minh City", 0));
+        Manila->setText(QApplication::translate("MainWindow", "Manila", 0));
+        Sydney->setText(QApplication::translate("MainWindow", "Sydney", 0));
+    } // retranslateUi
+
+};
+
+namespace Ui {
+    class MainWindow: public Ui_MainWindow {};
+} // namespace Ui
+
+QT_END_NAMESPACE
+
+#endif // UI_MAINWINDOW_H
