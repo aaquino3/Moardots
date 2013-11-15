2ec8b22d76b93a196c24efed1e77b5440ef300e2
aaquino3 <aquino.ardee@gmail.com>
2013-11-14 23:52:02 -0800

    Restoring All Files...

--- PandemicSource_CPP/QT_PandemicProject/README.txt
+++
@@ -1,6 +0,0 @@
-This is where we will be doing the QT project.
-After any update or changes please tell everyone on Google+.
-Add as many comments as you can to the push in git so that everyone knows what is going on.
-Everyone needs to download QT 5.1.1 here :
-
-http://qt-project.org/downloads
--- main.cpp
+++ main.cpp
@@ -1,16 +1,72 @@
-#include "mainwindow.h"
-#include <QLabel>
-#include <QGraphicsScene>
-#include <QGraphicsView>
-#include <QGraphicsPixmapItem>
-#include <QApplication>
+#include <iostream>
+#include <string>
+#include <ctime>
+#include <cstdlib>
+#include "Deck.h"
+#include "Card.h"
+#include "Player.h"
+#include "City.h"
+#include "Hand.h"
+#include "File.h"
+#include "Model.h"
+#include "View.h"
+#include "Controller.h"
+
 using namespace std;
 
-int main(int argc, char *argv[])
+int main()
 {
-    QApplication a(argc, argv);
-    MainWindow w;
-    w.show();
 
-    return a.exec();
+	// random seeder It's only called once in main
+	srand(unsigned(time(0)));
+
+	// Model View Controller
+	Model model;
+	View view;
+	Controller controller(model, view);
+
+	controller.updateIntro();
+	controller.updateGetNumOfPlayers();
+	controller.updateDisplayNumOfPlayers();
+
+	/*
+	//checking if player works
+    cout << endl;
+    Player getInfo("Ardee", "Nerd");
+	*/
+
+	/*
+    int sizeOfDeck = 7;
+    Deck roleDeck(sizeOfDeck);
+    Card temp;
+    roleDeck.readInCards("roleCards.txt", '|');
+	roleDeck.shuffle();
+    for(int i = 0; i < sizeOfDeck; i++)
+    {
+        temp = roleDeck.dealCard();
+        string type = temp.getType();
+        string name = temp.getName();
+        string description = temp.getDescription();
+        cout << "\ncard delt\n\n" << endl;
+        cout << "Type: " << type << endl;
+        cout << "name: " << name << endl;
+        cout << "Description: \n\n" << description << endl;
+    }
+	*/
+	/* this was a test for new methods in File, planning on updating the way it
+		reads the file
+    File *file = new File("countTest.txt");
+	int wordCount = file->countWords();
+	string *str = file->getWords();
+
+    cout << "word count = " << file->countWords() << endl;
+
+	for(int i = 0; i < wordCount; i++)
+	{
+		cout << str[i] << endl;
+	}
+	delete file;
+	*/
+	getchar();
+    return 0;
 }
