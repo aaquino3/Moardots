a03548fd29cd8f592446d91811fcf6afc340c099
ED <reyone59@gmil.com>
2013-11-21 14:19:29 -0800

    new File Class for input and output returns int's for errors/EOF depending on Method

--- File.cpp
+++ File.cpp
@@ -1,64 +1,129 @@
+/*
+ * File.cpp
+ *
+ *  Created on: Nov 19, 2013
+ *      Author: tech
+ */
+
 #include "File.h"
 
-File::File()
+
+
+int File::openRead()
 {
-    fin = 0;
-    fout = 0;
+	fin.open(readFileName.c_str());
+
+	if(fin.is_open())
+		return 0;
+
+	return -1;
 }
 
-void File::setReadFileName(string fileName)
+
+int File::openWrite()
 {
-    readFileName = fileName;
+	fout.open(writeFileName.c_str());
+
+	// check if file opened
+	if(fout.is_open())
+		return 0;
+
+	return -1;
 }
 
-void File::setWriteFileName(string fileName)
+void File::setReadFileName(string readFileName)
 {
-    writeFileName = fileName;
+	this->readFileName = readFileName;
+}
+
+void File::setWriteFileName(string writeFileName)
+{
+	this->writeFileName = writeFileName;
 }
 
 string File::getReadFileName()
 {
-    return readFileName;
+	return readFileName;
 }
 
 string File::getWriteFileName()
 {
-    return writeFileName;
+	return writeFileName;
 }
 
-string File::readLine()
+int File::readLine()
 {
-    string temp;
-    getline(*fin,temp);
-    return temp;
+	// check to see if file is open
+	if(fin.is_open() == false)
+		return -1;
+
+	string temp = "";
+
+	getline(fin, temp);
+
+	// check if getline() read something will return 0 if it did
+	if(fin)
+	{
+		lineRead = temp;
+		return 0;
+	}
+
+	// if it read EOF(end of file) or some other read error it will return -1
+	return -1;
 }
 
-void File::writeLine(string data)
+string File::getLine()
 {
-    *fout << data << '\n';
+	return lineRead;
 }
 
-void File::writeLine(int data)
+int File::writeLine(string line)
 {
-    *fout << data << '\n';
+	// checks to see if file is open
+	if(fout.is_open() == false)
+		return -1;
+
+	fout << line << endl;
+
+	return 0;
 }
 
-void File::writeLine(bool data)
+int File::closeRead()
 {
-    *fout << data << '\n';
+	// check if file is open
+	if(fin.is_open() == true)
+		fin.close();
+
+	// check again to see if fin is closed
+	if(fin.is_open() == false)
+		return 0;
+
+	// return -1 on fail
+	return -1;
+}
+
+int File::closeWrite()
+{
+	// check if file is open
+	if(fout.is_open() == true)
+		fout.close();
+
+	// check again if fout is closed
+	if(fout.is_open() == false)
+		return 0;
+
+	// return -1 on fail
+	return -1;
 }
 
 File::~File()
 {
-    if(fin != 0)
-    {
-        delete fin;
-        fin = 0;
-    }
 
-    if(fout != 0)
-    {
-        delete fout;
-        fout = 0;
-    }
+	// check if any file is open if it is then close them
+
+	if(fin.is_open() == true)
+		closeRead();
+
+	if(fout.is_open() == true)
+		closeWrite();
 }
--- File.h
+++ File.h
@@ -1,58 +1,86 @@
-#ifndef FILE_H
-#define FILE_H
-#include <iostream>
+/*
+ * File.h
+ *
+ *  Created on: Nov 19, 2013
+ *  Notes:
+ *  + recreated this class because first version was from prototype/ not complete
+ *  + currently only overwrites output files / cannot append
+ *
+ *	description:
+ *	Open files to read or write. some methods return int's for error's
+ *
+ *	errors include:
+ *	-1 could not open file
+ *	-2 reached end of file or error reading file I need to do this because
+ *	   string can't detect End Of File (EOF);
+ *
+ *  use:
+ *  1. create the File object
+ *  2. set the file name for reading or writing
+ *  3. open the file for reading or writing
+ *  4. read or write to file
+ *	5. close files or delete object
+ *
+ *  code example for reading:
+ *  File file;
+ *	file.setReadFileName("/home/tech/Downloads/CPPWorkspace/FileReader/src/test.txt");
+ *	file.setWriteFileName("/home/tech/Downloads/CPPWorkspace/FileReader/src/out.txt");
+ *
+ *	// print out file locations
+ *	cout << "Read file = " << file.getReadFileName() << endl;
+ *	cout << "Write file = " << file.getWriteFileName() << endl;
+ *
+ *	string temp;
+ *
+ *	if(file.readLine() != 0)
+ *		cout << "Error file not open" << endl;
+ *	// open file
+ *	if(file.openRead() == -1)
+ *		cout << "Error opening file" << endl;
+ *
+ *	if(file.readLine() == 0)
+ *		temp = file.getLine();
+ *
+ *	cout << "line read: " << temp << endl;
+ *
+ *	// close read file
+ *	file.closeRead();
+ *
+ *	file.openWrite();
+ *
+ *	file.writeLine("Test");
+ *  file.closeWrite();
+ *
+ */
+
+#ifndef FILE_H_
+#define FILE_H_
+#include <stdexcept>
 #include <string>
 #include <fstream>
-
 using namespace std;
 
-class File
-{
 
-private:
-    ifstream *fin;
-    ofstream *fout;
-    string readFileName;
-    string writeFileName;
+class File {
+	ifstream fin;
+	ofstream fout;
+	string readFileName;
+	string writeFileName;
+	string lineRead;
 
 public:
-    File();
-    ~File();
-    void setReadFileName(string);
-    void setWriteFileName(string);
-    string getReadFileName();
-    string getWriteFileName();
-    string readLine();
-    void writeLine(string);
-    void writeLine(int);
-    void writeLine(bool);
-    void readOpen();
-    void wrtieOpen();
-    void readClose();
-    void writeClose();
-
-
-    /* remaking class since it was just a protoype for
-     * console version
-     * Old code
-     *
-    private:
-        ifstream fin;
-        string errorOpenTxt;
-        string errorEofTxt;
-        bool errorLog;
-    public:
-        File(string);
-        ~File();
-        void fileOpen(string);
-        void fileClose();
-        string getStr();
-        // will read untill a delimiter character
-        string getStr(char);
-        void setErrorLog(bool);
-        bool eof();
-        int countWords();
-        string* getWords();
-        */
+	~File();						// will close open readFile or writeFile
+	int openRead();					// opens a read file, returns 0 on success or -1 on fail
+	int openWrite();				// opens a write file, returns 0 on success or -1 on fail
+	void setReadFileName(string);	// sets the file Name for reading
+	void setWriteFileName(string); 	// set the file Name for writing
+	string getReadFileName();		// gets the file Name for reading
+	string getWriteFileName();		// gets the file Name for writing
+	int readLine();					// reads a line returns 0 on success or -1 on file not open / -2 on EOF/error
+	string getLine();				// gets the last read string
+	int writeLine(string);			// writes a line to the write file, return -1 on fail
+	int closeRead();				// closes read file returns -1 on fail
+	int closeWrite();				// closes write file return -1 on fail
 };
-#endif // FILE_H
+
+#endif /* FILE_H_ */
