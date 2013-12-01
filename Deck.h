fca0883b66d7e4188c7378de0e1387902ccb3c46
ransackeld <kcransa@gmail.com>
2013-12-01 03:27:41 -0800

    Updated deck.h with shuffles methods
    Shuffle

--- Deck.h
+++ Deck.h
@@ -1,52 +1,48 @@
 #ifndef DECK_H
 #define DECK_H
 
+
 #include <string>
+#include <ctime>
+#include <cstdlib>
 #include "card.h"
-#include <deque>
-#include <algorithm>
 using namespace std;
-class Deck
+
+class deck
 {
     private:
-		/*
-		 * using a dequeue because its a doubly linked list
-		 * allowing use to cards from the top and bottom of the deck
-		 * as well as place cards on the top or bottom of the deck.
-		 */
-        deque <card> cards;
+        card Pdeck[numCards];
         int type;               // this is the type of deck, player or infection. Effects what kind it builds
-        int size;
-        void buildDeck();    // builds the appropriate deck based on the type input, player never needs to use this, only called by constructor.
+        int numCards;
+        void builddeck();    // builds the appropriate deck based on the type input, player never needs to use this, only called by constructor.
     public:
-        Deck(int);
-        ~Deck();
-
+        deck(int);
+        ~deck();
+	void shufflePlayerDeck();
         card dealCard();
         void shuffle();
-        bool isEmpty();
-        void putCardOnBottom(card);
-        void putCardOnTop(card);
+	bool isEmpty() { return !numCards; }
+	int getDeckSize() { return numCards; }
 };
 
 // Constructor to initialize deck data
-Deck::Deck(int deckType)
+deck::deck(int deckType)
 {
     type = deckType;    // 0 = player, anything else = infection
-    //buildDeck();    // build deck by type
+    builddeck();		// build deck by type
     shuffle();          // shuffle deck after building it.
 }
 
 // Destructor erases deque
-Deck::~Deck()
+deck::~deck()
 {
 	// is creating problems
 	//cards.erase(cards.begin(), cards.end());
     delete this;
 }
-/*
-void Deck::buildDeck() {
-    string cityNames[48] = {}; // need to make an array of strings with the names of every city
+
+void deck::builddeck() {
+    string cityNames[48] = {"Atlanta", "Washington", "Toronto", "Chicago"}; // need to make an array of strings with the names of every city
 
     string eventNames[5] = {"Airlift", "One Quiet Night", "Forecast", "Government Grant", "Resilient Population"};
 
@@ -58,68 +54,140 @@ void Deck::buildDeck() {
     eventDescriptions[4] = "Take a card from the infection Discard Pile and remove it from the game.";
     string epidemicDescription = ""; // can be handled during output to GUI
     if(type == 0) {     // player deck
-        size = 59;
-        for(int i = 1; i <= 48; i++) {
-            card * temp = new card(true, cityNames[i-1], "", 0, i);     // add all of the city cards
-            cards.push_front(temp);
+        numCards = 59;
+        for(int i = 0; i < 48; i++) {
+            card * temp = new card(true, cityNames[i], "", 0, i);     // add all of the city cards
+            Pdeck[i] = *temp;
         }
-        for(int i = 1; i <= 5; i++) {
-            card * temp = new card(true, eventNames[i-1], eventDescriptions[i-1], i, 0);    // add the event cards
-            cards.push_front(temp);
+        for(int i = 0; i < 4; i++) {
+            card * temp = new card(true, eventNames[i], eventDescriptions[i], i, 0);    // add the event cards
+            Pdeck[i+49] = *temp;
         }
         for(int i = 0; i < 4; i++) {
             card * temp = new card(true, "epidemic", epidemicDescription, 6, 0);        // add the epidmic cards
-            cards.push_front(temp);
+            Pdeck[i+54] = *temp;
         }
     } else {
-        size = 48;
-        for(int i = 1; i <= 48; i++) {
-            card * temp = new card(false, cityNames[i-1], "", 0, i);     // add all of the city cards
-            cards.push_front(temp);
-        }
+        numCards = 48;
+        for(int i = 0; i < 48; i++) {
+            card * temp = new card(false, cityNames[i], "", 0, i);     // add all of the city cards
+            Pdeck[i] = *temp;
+        }shuffle();//shuffles the city cards
     }
 }
-*/
+
 // will deal a card and remove it from the deck.
-card Deck::dealCard()
+card deck::dealCard()
 {
-	card temp = cards.front();
-	cards.pop_front();
+	card temp = Pdeck[numCards-1];
+	numCards--;
 	return temp;
 }
 
-// check to see if deck is empty return a boolean
-bool Deck::isEmpty()
-{
-	return cards.empty();
-}
-
 /*
-	will shuffle the deck using algorithm random_shuffle
-	to shuffle a dequeue holding the cards
-
     needs to be altered with a custom shuffle method for the player deck.
     The player deck requires that an epidemic card is placed randomly in every 10 draws.
 */
-void Deck::shuffle()
+void deck::shuffle()
 {
-	random_shuffle(cards.begin(), cards.end());
+	srand( time(0) );
+	if(!isEmpty())
+	{
+		int random = rand()%numCards;
+		card tempDeck[numCards];
+		for( int i = 0; i < numCards; i++)
+		{
+			card tempDeck[i] = Pdeck[random-1];
+		}
+		for( int i = 0; i < numCards; i++)
+			Pdeck[i] = tempDeck[i];
+	}
 }
 
-/*
- * places a card on the bottom of deck
- */
-void Deck::putCardOnBottom(card Dcard)
+void deck::shufflePlayerDeck()
 {
-	cards.push_back(Dcard);
+	if(!isEmpty())
+	{
+		int j = 0;
+		int random = rand()%numCards;
+		card tempE[5];
+		card tempD[numCards];
+		card pileA[11];
+		card pileB[12];
+		card pileC[12];
+		card pileD[12];
+		card pileE[12];
+	
+		for(int i = 0; i<numCards; i++) //removing epidemic cards
+		{		
+			if(Pdeck[i].getName()) == "epidemic")
+			{
+				tempE[j] = Pdeck[i];
+				j++;
+			}
+			else
+			{
+				tempD[i-j] = Pdeck[i];
+			}
+		}
+		for(int i = 0; i<numCards; i++) //shuffling deck without epidemic cards
+		{
+			Pdeck[i] = tempD[i];
+		}
+		shuffle();
+		
+		for(int i = 0; i<10; i++) // splitting deck and adding in epidemic cards
+		{
+			pileA[i] = Pdeck[i];
+		}
+		pileA[11] = tempE[0];
+		for(int i = 0; i<11; i++) 
+		{
+			pileB[i] = Pdeck[i+10];
+		}
+		pileB[12] = tempE[1];
+		for(int i = 0; i<11; i++) 
+		{
+			pileC[i] = Pdeck[i+10+11];
+		}
+		pileC[12] = tempE[2];
+		for(int i = 0; i<11; i++) 
+		{
+			pileD[i] = Pdeck[i+10+11+11];
+		}
+		pileD[12] = tempE[3];
+		for(int i = 0; i<11; i++) 
+		{
+			pileE[i] = Pdeck[i+10+11+11+11];
+		}
+		pileE[12] = tempE[4];
+
+		//stacking deck back together
+		for(int i = 0; i<11; i++)
+		{
+			pDeck[i] = pileA[i];
+		}
+		for(int i = 0; i<12; i++) 
+		{
+			Pdeck[i+11] = pileB[i];
+		}
+		for(int i = 0; i<12; i++) 
+		{
+			Pdeck[i+11+12] = pileC[i];
+		}
+		for(int i = 0; i<12; i++) 
+		{
+			Pdeck[i+11+12+12] = pileD[i];
+		}
+		for(int i = 0; i<12; i++) 
+		{
+			Pdeck[i+11+12+12+12] = pileE[i];
+		}
+	}
 }
 
-/*
- * places a card on the top of deck
- */
-void Deck::putCardOnTop(card Dcard)
-{
-	cards.push_front(Dcard);
-}
+
+
+
 
 #endif // DECK_H
